cd24332c08597597fb7f940562a63c1e995cc511
FabioH7 <113882125+FabioH7@users.noreply.github.com>
2023-04-13 17:31:35 +0200

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,3 +1,9 @@
+
+from flask import Blueprint
+
+app_views = Blueprint('api', __name__, url_prefix='/api/v1')
+
+
 from api.v1.views.users import *
 from api.v1.views.amenities import *
 from api.v1.views.cities import *
@@ -5,6 +11,3 @@ from api.v1.views.states import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.index import *
-from flask import Blueprint
-
-app_views = Blueprint('api', __name__, url_prefix='/api/v1')
